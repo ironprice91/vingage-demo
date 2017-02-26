7503829d7e2269d30e687f28452913132b072f13
Michael Canlas <michael.canlas428@gmail.com>
2017-02-26 02:56:25 -0500

    import video and noteHolder into app.js

--- public/src/components/app.js
+++ public/src/components/app.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import Video from '../containers/video';
+import NoteHolder from '../containers/noteHolder';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>React simple starter</div>
+        <Video/>
+        <NoteHolder/>
+      </div>
     );
   }
 }
--- public/src/containers/noteHolder.js
+++ public/src/containers/noteHolder.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Note from './note';
 
 export default class NoteHolder extends Component {
   render() {
     return (
-      <div>This is the parent container of notes</div>
+      <div>
+        <div>This is the parent container of notes</div>
+        <Note></Note>
+      </div>
     );
   }
 }
