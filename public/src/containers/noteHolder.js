d503547a5245f93e22dd269e21ceb7695e646a3d
Michael Canlas <michael.canlas428@gmail.com>
2017-02-26 02:48:57 -0500

    add container files

---
+++ public/src/containers/noteHolder.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class NoteHolder extends Component {
+  render() {
+    return (
+      <div>This is the parent container of notes</div>
+    );
+  }
+}
---
+++ public/src/containers/video.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Video extends Component {
+  render() {
+    return (
+      <div>This is where the video will live!</div>
+    );
+  }
+}
