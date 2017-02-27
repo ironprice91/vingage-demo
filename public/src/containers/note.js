e984fc8346d5f461671883b8cb214fd16cabd39f
canlasm <michael.canlas@foxnews.com>
2017-02-27 15:13:05 -0500

    add functionality to toggle edit note and update note + pass callback in saveEditedNote to untoggle edit note

--- public/src/containers/note.js
+++ public/src/containers/note.js
@@ -4,18 +4,46 @@ import axios from 'axios';
 export default class Note extends Component {
   constructor(props) {
       super(props);
+      this.state = {
+          "edit": false,
+          "noteText": props.note
+      };
+  }
+
+  toggleEdit() {
+    if ( !this.state.edit ) {
+      this.setState({"edit": true});
+    } else {
+      this.setState({"edit": false});
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      "noteText": e.target.value
+    });
   }
 
   render() {
     let noteID = this.props.id;
+    let note = this.props.note;
+    let noteMarkup = (this.state.edit) ? [
+      <div>
+        <p><span>Editing Note @ {this.props.displayTime}</span></p>
+        <form onSubmit={this.props.edit.bind(this, this.toggleEdit.bind(this))} data-video-id={this.props.videoID} data-note-id={`${noteID}`}>
+          <input name="" id="" cols="30" rows="3" value={this.state.noteText} onChange={this.handleChange.bind(this)}></input>
+        </form>
+      </div>
+    ] : [<p><span>{this.props.displayTime}</span> - {this.state.noteText}</p>];
 
     return (
       <div id={`note-id-${noteID}`}>
         <li
           data-videoid={this.props.videoID}
           data-note-time={this.props.time}>
-          {this.props.note} @ {this.props.displayTime}
-          <button data-video-id={this.props.videoID} data-note-id={`${noteID}`} onClick={this.props.delete}>delete</button>
+          {noteMarkup}
+          <button data-video-id={this.props.videoID} data-note-id={`${noteID}`} onClick={this.props.delete}>Delete</button>
+          <button data-video-id={this.props.videoID} data-note-id={`${noteID}`} onClick={this.toggleEdit.bind(this)}>Edit</button>
         </li>
       </div>
     );
--- public/src/containers/noteHolder.js
+++ public/src/containers/noteHolder.js
@@ -20,7 +20,7 @@ export default class NoteHolder extends Component {
       let videoID = this.props.video._id;
 
       this.props.video.notes.forEach((note) => {
-          noteMarkup.push(<Note videoID={videoID} key={note._id} note={note.note} displayTime={note.displayTime} time={note.time} id={note._id} delete={this.deleteNote.bind(this)}></Note>);
+          noteMarkup.push(<Note videoID={videoID} key={note._id} note={note.note} displayTime={note.displayTime} time={note.time} id={note._id} delete={this.deleteNote.bind(this)} edit={this.saveEditedNote.bind(this)}></Note>);
       });
 
       this.sortNoteTime(noteMarkup);
@@ -42,7 +42,7 @@ export default class NoteHolder extends Component {
         note = res.data;
 
         this.setState({
-          "notes": this.state.notes.concat([<Note key={note._id} note={note.note} displayTime={note.displayTime} time={note.time} id={note._id} delete={this.deleteNote.bind(this)}></Note>]).sort((a,b) => a.props.time > b.props.time),
+          "notes": this.state.notes.concat([<Note key={note._id} note={note.note} displayTime={note.displayTime} time={note.time} id={note._id} delete={this.deleteNote.bind(this)} edit={this.saveEditedNote.bind(this)}></Note>]).sort((a,b) => a.props.time > b.props.time),
           "note": ""
         });
 
@@ -67,6 +67,21 @@ export default class NoteHolder extends Component {
       if ( err ) { throw err; }
     });
 
+  }
+
+  saveEditedNote(callback, e) {
+
+      e.preventDefault();
+      let videoID = e.target.getAttribute("data-video-id");
+      let noteID = e.target.getAttribute("data-note-id");
+      let newNote = e.target.firstChild.value;
+      let editURL = `http://localhost:6060/api/video/${videoID}/note/${noteID}`;
+
+      axios.post(editURL, {"note": newNote}).then((res) => {
+        if ( typeof callback === "function" ) { callback(); }
+      }).catch((err) => {
+        if ( err ) { throw err; }
+      });
   }
 
   convertToDisplayTime(float) {
